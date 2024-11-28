e0dc14f146dc5d90248cf518af327298d7ea4ab9
Phan Duc Minh <23520950@gm.uit.edu.vn>
2024-11-28 21:27:52 +0700

    update test contract size with web3.js

--- test/contract-size.js
+++ test/contract-size.js
@@ -1,16 +1,30 @@
 const { expect } = require("chai");
+const abi = require('../artifacts/contracts/Contract.sol/SeedToken.json').abi;
+const bytecode = require('../artifacts/contracts/Contract.sol/SeedToken.json').bytecode;
 
 const maxContractSize = 24*1024;
 
 describe("Compiled Smart Contracts Size Test", function () {
     it("Should deploy the SeedToken contract", async function () {
-        const SeedToken = await ethers.getContractFactory("SeedToken");
-        [deployer] = await ethers.getSigners();
-        const seedToken = await SeedToken.deploy(deployer.address, "Seed Token", "SEED");
-        await seedToken.waitForDeployment();
-        const deployedAddress = await seedToken.getAddress();
+        //const SeedToken = await ethers.getContractFactory("SeedToken");
+
+        const SeedToken = new web3.eth.Contract(abi);
+        //[deployer] = await ethers.getSigners();
+        [deployer] = await web3.eth.getAccounts();
+
+        //const seedToken = await SeedToken.deploy(deployer.address, "Seed Token", "SEED");
+        //await seedToken.waitForDeployment();
+        const deployment = SeedToken.deploy({
+            data: bytecode,
+            arguments: [deployer, "Seed Token", "SEED"]
+        });
+        const receipt = await deployment.send({
+            from: deployer
+        });
+        //const deployedAddress = await seedToken.getAddress();
+        const deployedAddress = receipt.options.address;
         expect(deployedAddress).to.exist;
-        const deployedCode = await seedToken.getDeployedCode();
+        const deployedCode = await web3.eth.getCode(deployedAddress);
         const size = (deployedCode.length - 2)/2;
         console.log('    SeedToken contract size: ' + size + ' bytes');
         expect(size <= maxContractSize).to.be.true;
