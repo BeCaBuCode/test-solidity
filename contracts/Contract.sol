95c96ed783ac14311a6591f7718838d8964717c6
Phan Duc Minh <23520950@gm.uit.edu.vn>
2024-11-28 20:27:46 +0700

    Add simple ERC-20 cryptocurrency

---
+++ contracts/Contract.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+error Unauthorized();
+
+contract SeedToken is ERC20 {
+    address public owner;
+
+    modifier isOwner() {
+      if (msg.sender == owner) {
+        _;
+      } else {
+        revert Unauthorized();
+      }
+    }
+
+    constructor(
+        address ownerAddress,
+        string memory name,
+        string memory symbol 
+    ) ERC20 (name, symbol){
+        owner = ownerAddress;
+    }
+
+    function changeOwner(address newOwner) external isOwner {
+        owner = newOwner;
+    }
+
+    function mint(uint256 amountOfTokens) external isOwner {
+        _mint(msg.sender, amountOfTokens * 10 **uint(decimals()));
+    }
+}
