dea00ad5561fabe714c446f9120d8441d01bc47d
Phan Duc Minh <23520950@gm.uit.edu.vn>
2024-11-28 20:53:12 +0700

    Add contract test file using ether.js

---
+++ test/contract-size.js
@@ -0,0 +1,17 @@
+const { expect } = require("chai");
+
+const maxContractSize = 24*1024;
+
+describe("Compiled Smart Contracts Size Test", function () {
+    it("Should deploy the SeedToken contract", async function () {
+        const SeedToken = await ethers.getContractFactory("SeedToken");
+        [deployer] = await ethers.getSigners();
+        const seedToken = await SeedToken.deploy(deployer.address, "Seed Token", "SEED");
+        const deployedAddress = await seedToken.getAddress();
+        expect(deployedAddress).to.exist;
+        const deployedCode = await seedToken.getDeployedCode();
+        const size = (deployedCode.length - 2)/2;
+        console.log('    SeedToken contract size: ' + size + ' bytes');
+        expect(size <= maxContractSize).to.be.true;
+    });
+});
